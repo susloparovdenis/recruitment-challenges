a72e2d86315a42bc1d7d619d6e1ac34de1db8ebb
Denis <densus@gmail.com>
2018-05-08 00:32:11 +0200

    Added failing test

--- Refactoring.FraudDetection.Tests/FraudRadarTests.cs
+++ Refactoring.FraudDetection.Tests/FraudRadarTests.cs
@@ -60,7 +60,18 @@ namespace Payvision.CodeChallenge.Refactoring.FraudDetection.Tests
             result.Count().ShouldBeEquivalentTo(2, "The result should contains the number of lines of the file");
         }
 
-        
+        [TestMethod]
+        [DeploymentItem("./Files/TwoLinesEmailContainsPlusAndDot_FraudulentSecond.txt", "Files")]
+        public void CheckFraud_TwoLinesEmailContainsPlusAndDot_FraudulentSecond()
+        {
+            var result = ExecuteTest(Path.Combine(Environment.CurrentDirectory, "Files", "TwoLinesEmailContainsPlusAndDot_FraudulentSecond.txt"));
+            result.Should().NotBeNull("The result should not be null.");
+            result.Count().ShouldBeEquivalentTo(1, "The result should contains the number of lines of the file");
+            result.First().IsFraudulent.Should().BeTrue("The first line is not fraudulent");
+            result.First().OrderId.Should().Be(2, "The first line is not fraudulent");
+        }
+
+
 
         private static List<FraudRadar.FraudResult> ExecuteTest(string filePath)
         {
